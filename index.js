dbf3022a42061444bae44eab3e01b9eb48d0ffd3
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-26 23:27:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++){
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+
+  return names;
+}
+
+
+
+function tailsNeverFails() {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
