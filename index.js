74b67fbb490e7e2f45b6b141eb5e3f699b8da891
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-26 23:20:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 const names = ['Lisa', 'Kaitlin', 'Jan'];
 
-function printBadges() {
+function printBadges(names) {
   for (let i = 0; i < names.legnth; i++){
     console.log(`Welcome ${name[i]}! You are employee `)
   }
