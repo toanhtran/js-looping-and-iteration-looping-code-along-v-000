265919a3e773e47e88b9415714cc4ca7d1dd8771
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-26 23:22:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const names = ['Lisa', 'Kaitlin', 'Jan'];
 
 function printBadges(names) {
   for (let i = 0; i < names.length; i++){
-    console.log(`Welcome ${name[i]}! You are employee #${i + 1} `);
+    console.log(`Welcome ${name[i]}! You are employee #${i + 1}. `);
   }
 
   return names;
